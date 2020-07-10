44388565c9f52a8a84f9801fc7a7697a98d40896
Saeed Shareef <saeedhshareef@gmail.com>
2020-07-10 08:20:16 -0400

    loopPractice

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log("Connection established, Captain")
+
+
+var firstArray = [];
+for ( var i = 0; i < 10; i++) {
+        firstArray.push(i);
+}
+
+var secondArray = [] ;
+var j = 10 ;
+ while(j > 1)   {
+        j--;
+      secondArray.push(j);   
+}
+
+var thirdArray = [] ;
+for(k = 11; k <= 21; k +=2){
+    thirdArray.push(k);
+}
+
+var fourthArray = [] ;
+
+for(l = 15; l >= 1; l -= 2){
+    fourthArray.push(l)
+}
